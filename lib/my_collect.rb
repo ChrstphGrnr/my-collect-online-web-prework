c8adefa0b2b8a160e0fe1eb69bdf586c11fd162c
Christoph Gruener <christophgruener@me.com>
2019-01-29 22:39:20 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(collection)
   i = 0  
   modified_collection = []
   while i < modified_collection.length 
-    modified_collection << (yield collection[i])
+    modified_collection.push(yield collection[i])
     i += 1 
   end
   modified_collection
